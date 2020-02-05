5ceec72e5af3f25b582c155fe7b62c3b07e3b30d
Mateen <mateen.khan91@t-mobile.com>
2020-02-05 14:34:45 +0530

    first commit

---
+++ java/spring/oauth2_2/Oauth2Client/src/main/java/learning/spring/Oauth2Client/WebController.java
@@ -0,0 +1,18 @@
+package learning.spring.Oauth2Client;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.security.Principal;
+@Controller
+public class WebController {
+    @RequestMapping("/securedPage")
+    public String securedPage(Model model, Principal principal) {
+        return "securedPage";
+    }
+    @RequestMapping("/")
+    public String index(Model model, Principal principal) {
+        return "index";
+    }
+}
